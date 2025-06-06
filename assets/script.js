1599be7704735ea1e459a6e87201d66b397ef096
NAR <yyazok@gmail.com>
2025-06-06 15:20:20 +0300

    test

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const links = [
     { url: "https://devnar.github.io/sayidisi/", category: "Projects", type: "fav" },
     { url: "https://devnar.github.io/pitopi/", category: "Projects", type: "fav" },
     { url: "https://devnar.github.io/inh/", category: "Projects", type: "fav" },
-    { url: "https://excalidraw.com/", category: "Tools" },
+    { url: "https://excalidraw.com", category: "Tools" },
     { url: "https://obsidian.md/", category: "Tools" },
     { url: "https://www.todoist.com/", category: "Tools" },
     { url: "https://app.haikei.app/", category: "Tools" },
@@ -14,46 +14,44 @@ const links = [
     { url: "https://muetab.com/", category: "Tools" },
     { url: "https://typefully.com/minimal-twitter", category: "Tools" },
     { url: "https://www.modernwiki.app/", category: "Tools" },
-    { url: "https://uselumen.com/", category: "Tools" },
+    { url: "https://app.uselumen.com/", category: "Tools" },
     { url: "https://ionic.io/ionicons", category: "Tools" },
     { url: "https://boxicons.com/", category: "Tools" },
     { url: "https://lucide.dev/", category: "Tools" },
     { url: "https://tailwindui.com/", category: "Tools" },
     { url: "https://yarn.co/", category: "Tools", type: "none" },
-    { url: "https://www.dr.com.tr/kitap/1984/edebiyat/roman/dunya-roman/urunno=0000000064038?srsltid=AfmBOoo6xDD6_icAcVFG6jb7Qdi0Jug1T3ZBIU6gfP_zRzCut2JNg_Gh", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/kuskucu-somon/edebiyat/roman/bilim-kurgu/urunno=0001712159001?srsltid=AfmBOopUjcfkingDrMua3Egz2r76PR0wvvHZx1ajunizIn7USgDQIbar", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/cogunlukla-zararsiz/edebiyat/roman/bilim-kurgu/urunno=0001705121001?srsltid=AfmBOoqw8WbGECBls_TXlCOT11VWWrzTa4v7JcocbFkobzU7GDOTbDkw", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/elveda-ve-butun-o-baliklar-icin-tesekkurler/edebiyat/roman/bilim-kurgu/urunno=0001705122001?srsltid=AfmBOoog3wrnY8ekfqCIKblK2ZUCGXjJOFpuQhizTsqS3vjrMsR3_YpN", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/baslat/edebiyat/roman/fantastik/urunno=0000000373271?srsltid=AfmBOor0lfQBPTbbmaFDDHsbv5nfFxLhdCbkbjSp3OZJ8LsAgdRzLjaK", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/baslat-ready-player-two/edebiyat/roman/bilim-kurgu/urunno=0002006508001?srsltid=AfmBOorpGRUG9VCQc0QypgMDJMyusHH7su4sJgxtPPK9ySsoQKCOgqrZ", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/vakif/edebiyat/roman/bilim-kurgu/urunno=0001730588001?srsltid=AfmBOopO6EZwsGMvyL0PryYso7DRBwqZGWmeK7N0Z2s95XzWKvxRIg76", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/hayat-evren-ve-her-sey/edebiyat/roman/bilim-kurgu/urunno=0001705124001?srsltid=AfmBOorWFkwj-X2-ZU792glNYSDrvns-3orwgsV0nBG1-L6YrUJi5uNE", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/evrenin-sonundaki-restoran/edebiyat/roman/bilim-kurgu/urunno=0001705123001?srsltid=AfmBOoqkv4RyBoAtPW_W92Tv62eqBvVxTaYgA-yyQmOoBISV3brqhjQd", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/otostopcunun-galaksi-rehberi/edebiyat/roman/bilim-kurgu/urunno=0001705120001?srsltid=AfmBOopI3oQ7eSKf-uN5JWBIa_o-2cMjIR9hrrd0yeQJplVd3kU0C0aE", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/pal-sokagi-cocuklari-kucuk-boy/cocuk-ve-genclik/okul-cagi-6-10-yas/cocuk-klasik/urunno=0000000291906?srsltid=AfmBOop7thTgHsA1sE7pIMXwRFYYGcJML9iNyTBzvvchhUdJsgK1ksd0", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/haritali-adam/edebiyat/roman/korku-gerilim/urunno=0000000682540?srsltid=AfmBOoqWn7jNZBteSCrWe6nls90Tk28bR6wN0ynph9CAqBfjjR42dsSI", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/gece-yarisina-12-dakika-kala/cocuk-ve-genclik/genclik-10-yas/fantastikbilim-kurgu/urunno=0001683839001?srsltid=AfmBOoqZovudJbxyAS5snqD9jozesbqfxZdy3PyUYhmNho0IWjw2Jfdr", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/kucuk-hezarfenin-seruvenleri-1/salih-ozcelik/edebiyat/roman/turkiye-roman/urunno=0000000696782?srsltid=AfmBOopcAvzn9fwQN2FFsGJ4ox0uuyR0c51dKL60dyucSRpcihekZsza", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/Kitap/Beyin-Avcisi-1-Zindan-Adasi/Ahmet-Baki-Yerli/cocuk-ve-Genclik/Genclik-10-Yas/Roman-oyku/urunno=0001700513003?srsltid=AfmBOoo6uR6UBZR2uJyi8aBoF3KwJvRRgOofRnP6tN1WfsXgIjNXTNS-", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/beyin-avcisi-4-olum-ormani/ahmet-baki-yerli/cocuk-ve-genclik/genclik-10-yas/roman-oyku/urunno=0001772538001?srsltid=AfmBOooQ6KUwdlDA4yeICeN8_Bc0y79DRXjs9gsWC356Jauc2sD3c30b", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/yaratik-departmani/cocuk-ve-genclik/genclik-10-yas/fantastikbilim-kurgu/urunno=0000000713754?srsltid=AfmBOoqjovLbeUBxllITzqXUXYVvn64bX_JbMnObeuqoxoQiv6W_pSzX", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/superozorlar-1-ormandaki-cocuk/cocuk-ve-genclik/genclik-10-yas/fantastikbilim-kurgu/urunno=0001781937001?srsltid=AfmBOoopC6YzpgXm_GxWbW8t0iIdFcVeezv3qimEWcnCqB1j8h1ERdtn", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/kucuk-prens/cocuk-ve-genclik/okul-cagi-6-10-yas/cocuk-klasik/urunno=0000000628979?srsltid=AfmBOoo_10D9491lLEF_R8fRrqTOJp9klP6VRrCf7tE4R9ufMM1ln7_j", category: "Books", type: "writeless" },
-    {
-        url: "https://www.dr.com.tr/kitap/saftirik-gregin-gunlugu-4-iste-simdi-yandik/jeff-kinney/cocuk-ve-genclik/genclik-10-yas/roman-oyku/urunno=0000000351286?srsltid=AfmBOoqFaoj_YlOhLHlgWuNVPuXH6wlSMZxfl7KFKCUNFsLyIsOS94vq",
-        category: "Books",
-    },
-    {
-        url: "https://www.dr.com.tr/kitap/saftirik-gregin-gunlugu-3-turunun-son-ornegi/jeff-kinney/cocuk-ve-genclik/genclik-10-yas/roman-oyku/urunno=0000000327414?srsltid=AfmBOoqYGpxYJCOpBZVk6E5EGZCBsvTu0MZgTYx5jSzAgiHJHpcaPMV8",
-        category: "Books",
-    },
-    {
-        url: "https://www.dr.com.tr/kitap/saftirik-gregin-gunlugu-2-rodrick-kurallari/jeff-kinney/cocuk-ve-genclik/genclik-10-yas/roman-oyku/urunno=0000000311400?srsltid=AfmBOorAYy5ZnfPbCtEDgPfkuh2JvF10xPhMmjpmSlcrf4efsCcqTr7J",
-        category: "Books",
-    },
-    { url: "https://www.dr.com.tr/kitap/saftirik-gregin-gunlugu-1/jeff-kinney/cocuk-ve-genclik/genclik-10-yas/roman-oyku/urunno=0000000284528", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/halime-kaptan/cocuk-ve-genclik/genclik-10-yas/romanoyku/urunno=0000000349965?srsltid=AfmBOoqZpdqt10kLj2jYH9oUIvSZsBz5b9mziCayPaZSiBic8aLD-mUE", category: "Books", type: "writeless" },
-    { url: "https://www.dr.com.tr/kitap/acimak/edebiyat/roman/turk-klasik/urunno=0000000052555?srsltid=AfmBOopTwRpgYY4YaaZ7ShRlXnJ2DjGNbRcXsJVpd2IduiwGCKn1YCXw", category: "Books", type: "writeless" },
+    { url: "https://www.imdb.com/title/tt2861424", category: "TvSeries", type: "writeless" },
+    { url: "https://www.imdb.com/title/tt1710308", category: "TvSeries", type: "writeless" },
+    /*{ url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000064038-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001712159001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001705121001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001705122001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000373271-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0002006508001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001772519001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001730588001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001705124001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001705123001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001705120001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000291906-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000682540-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001683839001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000398506-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001747016001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000696782-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001772538002-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001700513003-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000713754-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0001781937001-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://img.kitapyurdu.com/v1/getImage/fn:1009425/wh:true/wi:220", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000628979-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000351286-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000327414-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000311400-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000284528-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000411500-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000349965-1.jpg", category: "Books", type: "writeless" },
+    { url: "https://i.dr.com.tr/cache/500x400-0/originals/0000000052555-1.jpg", category: "Books", type: "writeless" },*/
 ];
 
 const categoryColorMap = {
@@ -63,80 +61,119 @@ const categoryColorMap = {
     Books: "#fb7185",
 };
 
-const container = document.getElementById("links");
+(async () => {
+    const container = document.getElementById("links");
 
-links.forEach(async (link) => {
-    const res = await fetch(`https://api.microlink.io/?url=${encodeURIComponent(link.url)}`);
-    const data = await res.json();
+    const promises = links.map(async (link) => {
+        const res = await fetch(`https://api.microlink.io/?url=${encodeURIComponent(link.url)}`);
+        const data = await res.json();
 
-    const title = data.data.title || link.url;
-    const description = data.data.description || "";
-    const image = data.data.image?.url;
-    const fav = data.data.logo?.url;
+        console.log(data);
 
-    const card = document.createElement("a");
-    card.className = "card relative thumbnail-shadow flex aspect-auto min-w-0 cursor-pointer flex-col gap-4 overflow-hidden rounded-xl bg-zinc-950 p-4 transition-colors duration-300 hover:bg-gray-900";
-    card.href = link.url;
-    card.target = "_blank";
-    card.rel = "noopener noreferrer";
+        const title = data.data.title || link.url;
+        const description = data.data.description || "";
+        const image = data.data.image?.url;
+        const fav = data.data.logo?.url;
 
-    // Etiket
-    const tag = document.createElement("div");
-    tag.textContent = link.category;
-    tag.className = "category absolute top-3 right-3 rounded-md bg-[#23b5b5] px-2 py-0.5 text-xs font-medium text-black";
-    tag.style.backgroundColor = categoryColorMap[link.category] || "#ccc";
-    card.appendChild(tag);
+        if (link.type === "writeless") {
+            const card = document.createElement("a");
+            card.href = link.url;
+            card.target = "_blank";
+            card.rel = "noopener noreferrer";
+            card.className = `
+      card relative flex min-w-0 cursor-pointer flex-col justify-end
+      overflow-hidden rounded-xl bg-zinc-900 text-white aspect-[16/9]
+      shadow-md transition-transform duration-300  hover:aspect-[10/16]
+    `;
+    const bgUrl = image || fav
+            card.style.backgroundImage = `url(${bgUrl})`;
+            card.style.backgroundSize = "cover";
+            card.style.backgroundPosition = "center";
+            const tag = document.createElement("div");
+            tag.textContent = link.category;
+            tag.className = "category absolute top-3 right-3 rounded-md px-2 py-0.5 text-xs font-medium text-black";
+            tag.style.backgroundColor = categoryColorMap[link.category] || "#ccc";
+            card.appendChild(tag);
 
-    // Görsel varsa
-    if (image && link.type != "fav" && link.type != "none") {
-        const imgWrap = document.createElement("span");
-        imgWrap.className = "aspect-1200/630 overflow-hidden rounded-lg";
-        const img = document.createElement("img");
-        img.src = image;
-        img.alt = title;
-        img.className = "animate-reveal aspect-1200 rounded-lg bg-cover bg-center bg-no-repeat object-cover mb-5";
-        img.loading = "eager";
-        img.width = 1200;
-        imgWrap.appendChild(img);
-        card.appendChild(imgWrap);
-    } else if (link.type != "none") {
-        const imgWrap = document.createElement("span");
-        imgWrap.className = "aspect-40/40 overflow-hidden rounded-lg";
-        const img = document.createElement("img");
-        img.src = fav;
-        img.alt = title;
-        img.className = "animate-reveal aspect-40 rounded-lg bg-cover bg-center bg-no-repeat object-cover mb-5";
-        img.loading = "eager";
-        img.width = 40;
-        imgWrap.appendChild(img);
-        card.appendChild(imgWrap);
-    }
+            const overlay = document.createElement("div");
+            overlay.className = `
+      absolute inset-0 bg-gradient-to-t from-black/70 to-transparent
+      flex flex-col justify-end p-4
+    `;
 
-    // Metin
-    const textBox = document.createElement("div");
-    textBox.className = "flex flex-col gap-1";
+            const haveatitle = title !== "No title";
+            const h2 = document.createElement("h2");
+            h2.className = "text-lg font-semibold line-clamp-2";
+            if (haveatitle) h2.textContent = title;
 
-    const h2 = document.createElement("h2");
-    h2.className = "line-clamp-4 text-lg leading-snug";
-    h2.textContent = title;
-    textBox.appendChild(h2);
+            overlay.appendChild(h2);
+            card.appendChild(overlay);
+            container.appendChild(card);
+        } else {
+            const card = document.createElement("a");
+            card.className = "card relative thumbnail-shadow flex aspect-auto min-w-0 cursor-pointer flex-col gap-4 overflow-hidden rounded-xl bg-zinc-950 p-4 transition-colors duration-300 hover:bg-gray-900";
+            card.href = link.url;
+            card.target = "_blank";
+            card.rel = "noopener noreferrer";
 
-    const domain = new URL(link.url).hostname.replace("www.", "");
-    const domainSpan = document.createElement("span");
-    domainSpan.className = "line-clamp-4 inline-flex items-center gap-1 text-sm text-gray-500";
-    domainSpan.innerHTML = `<i class="bx  bx-link"></i> ${domain}`;
-    textBox.appendChild(domainSpan);
+            const tag = document.createElement("div");
+            tag.textContent = link.category;
+            tag.className = "category absolute top-3 right-3 rounded-md px-2 py-0.5 text-xs font-medium text-black";
+            tag.style.backgroundColor = categoryColorMap[link.category] || "#ccc";
+            card.appendChild(tag);
 
-    if (description && link.type !== "writeless") {
-        const desc = document.createElement("span");
-        desc.className = "line-clamp-6 text-sm opacity-80";
-        desc.textContent = description;
-        textBox.appendChild(desc);
-    }
+            if (image && link.type !== "fav" && link.type !== "none") {
+                const imgWrap = document.createElement("span");
+                imgWrap.className = "aspect-1200/630 overflow-hidden rounded-lg";
+                const img = document.createElement("img");
+                img.src = image;
+                img.alt = title;
+                img.className = "animate-reveal aspect-1200 rounded-lg object-cover mb-5";
+                img.loading = "eager";
+                img.width = 1200;
+                imgWrap.appendChild(img);
+                card.appendChild(imgWrap);
+            } else if (link.type !== "none") {
+                const imgWrap = document.createElement("span");
+                imgWrap.className = "aspect-40/40 overflow-hidden rounded-lg";
+                const img = document.createElement("img");
+                img.src = fav;
+                img.alt = title;
+                img.className = "animate-reveal aspect-40 rounded-lg object-cover mb-5";
+                img.loading = "eager";
+                img.width = 40;
+                imgWrap.appendChild(img);
+                card.appendChild(imgWrap);
+            }
 
-    card.appendChild(textBox);
-    container.appendChild(card);
-});
+            const textBox = document.createElement("div");
+            textBox.className = "flex flex-col gap-1";
+
+            const h2 = document.createElement("h2");
+            h2.className = "line-clamp-4 text-lg leading-snug";
+            h2.textContent = title;
+            textBox.appendChild(h2);
+
+            const domain = new URL(link.url).hostname.replace("www.", "");
+            const domainSpan = document.createElement("span");
+            domainSpan.className = "inline-flex items-center gap-1 text-sm text-gray-500";
+            domainSpan.innerHTML = `<i class="bx bx-link"></i> ${domain}`;
+            textBox.appendChild(domainSpan);
+
+            if (description && link.type !== "writeless") {
+                const desc = document.createElement("span");
+                desc.className = "text-sm opacity-80";
+                desc.textContent = description;
+                textBox.appendChild(desc);
+            }
+
+            card.appendChild(textBox);
+            container.appendChild(card);
+        }
+    });
+
+    await Promise.all(promises); // Paralel çalıştır ve bitmesini bekle
+})();
 
 const btn = document.getElementById("scrollTopBtn");
 
